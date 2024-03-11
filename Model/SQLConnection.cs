c462bf106c3f6d021e16342c0e63a7dd8f1805e4
DatHocVietCode <td13052004@gmail.com>
2024-03-11 18:06:51 +0700

    Toi met

---
+++ Model/SQLConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WPF_Market.Model
+{
+    public static class SQLConnection
+    {
+        public static SqlConnection conn = new SqlConnection(Properties.Settings.Default.TraoDoiMuaBan);
+    }
+}
